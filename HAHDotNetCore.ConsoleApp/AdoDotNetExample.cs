93b09637d8f2ab8820503746466953668557eafe
hhtun137 <htunaunghlaing199@gmail.com>
2024-06-12 23:45:52 +0630

    writing ado.net create

---
+++ HAHDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HAHDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder _stringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "localhost",
+            InitialCatalog = "HAHDotNetCore",
+            UserID = "SA",
+            Password = "Password123"
+        };
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+            Console.WriteLine("Connection Open.");
+
+            string query = "select * from tbl_blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+
+            connection.Close();
+            Console.WriteLine("Connection Close.");
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+
+        public void create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[tbl_Blog]
+                            ([BlogTitle],
+                            [BlogAuthor],
+                            [BlogContent])
+                            values
+                            (@BlogTitle,
+                            @BlogAuthor,
+                            @BlogContent)";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            sqlCommand.Parameters.AddWithValue("@BlogTitle", title);
+            sqlCommand.Parameters.AddWithValue("@BlogAuthor", author);
+            sqlCommand.Parameters.AddWithValue("@BlogContent", content);
+            int result = sqlCommand.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Insert Success." : "Insert Fail.";
+            Console.WriteLine(message);
+        }
+    }
+}
+
--- HAHDotNetCore.ConsoleApp/Program.cs
+++ HAHDotNetCore.ConsoleApp/Program.cs
@@ -1,35 +1,11 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using HAHDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "localhost";
-stringBuilder.InitialCatalog = "HAHDotNetCore";
-stringBuilder.UserID = "SA";
-stringBuilder.Password = "Password123";
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.create("test 11", "test author", "test content");
 
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-connection.Open();
-Console.WriteLine("Connection Open.");
-
-string query = "select * from tbl_blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dataTable = new DataTable();
-sqlDataAdapter.Fill(dataTable);
-
-connection.Close();
-Console.WriteLine("Connection Close.");
-
-foreach (DataRow dr in dataTable.Rows)
-{
-    Console.WriteLine("Blog Id => " + dr["BlogId"]);
-    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
-    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
-    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
-    Console.WriteLine("-----------------------------------");
-}
-
-Console.ReadKey();
+Console.ReadLine();
 
