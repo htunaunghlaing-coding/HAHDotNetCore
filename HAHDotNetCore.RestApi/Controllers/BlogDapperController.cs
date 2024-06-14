8635abf728351f7b82e70cafc65290cc5aee93f1
hhtun137 <htunaunghlaing199@gmail.com>
2024-06-14 23:32:54 +0630

    writing asp.net core web api dapper read

---
+++ HAHDotNetCore.RestApi/Controllers/BlogDapperController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HAHDotNetCore.RestApi.Controllers;
+
+[Route("api/[controller]")]
+public class BlogDapperController : Controller
+{
+    [HttpGet]
+    public IActionResult getBlogs()
+    {
+        string query = "select * from tbl_blog";
+
+        using IDbConnection db = new SqlConnection(SqlConnectionString.StringBuilder.ConnectionString);
+        List<BlogModel> lists = db.Query<BlogModel>(query).ToList();
+
+        return Ok(lists);
+    }
+}
+
