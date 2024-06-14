cea30d1eb92972f54d6664d3d8c6ef2246ecf19f
hhtun137 <htunaunghlaing199@gmail.com>
2024-06-14 19:07:20 +0630

    testing asp.net core web api

---
+++ HAHDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HAHDotNetCore.RestApi.Controllers;
+
+[Route("api/[controller]")]
+public class BlogController : Controller
+{
+    [HttpGet]
+    public IActionResult Read()
+    {
+        return Ok("Read");
+    }
+
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Ok("Create");
+    }
+
+    [HttpPut]
+    public IActionResult Update()
+    {
+        return Ok("Update");
+    }
+
+    [HttpPatch]
+    public IActionResult Patch()
+    {
+        return Ok("Patch");
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok("Delete");
+    }
+}
+
