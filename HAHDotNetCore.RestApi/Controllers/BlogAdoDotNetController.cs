20e10fe00d4c720ee9a1abc34d2bdc37c5a3a23d
hhtun137 <htunaunghlaing199@gmail.com>
2024-06-15 15:47:19 +0630

    writing asp.net core web api ado.net edit

--- HAHDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
+++ HAHDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -28,18 +28,59 @@ public class BlogAdoDotNetController : Controller
 
         connection.Close();
 
-        List<BlogModel> blogs = new List<BlogModel>();
-        foreach (DataRow dr in data.Rows)
+        //List<BlogModel> blogs = new List<BlogModel>();
+        //foreach (DataRow dr in data.Rows)
+        //{
+        //    BlogModel blog = new BlogModel();
+        //    blog.BlogId = Convert.ToInt32(dr["BlogId"]);
+        //    blog.BlogTitle = Convert.ToString(dr["BlogTitle"]);
+        //    blog.BlogAuthor = Convert.ToString(dr["BlogAuthor"]);
+        //    blog.BlogContent = Convert.ToString(dr["BlogContent"]);
+        //    blogs.Add(blog);
+        //}
+
+        List<BlogModel> blogs = data.AsEnumerable().Select(dr => new BlogModel
         {
-            BlogModel blog = new BlogModel();
-            blog.BlogId = Convert.ToInt32(dr["BlogId"]);
-            blog.BlogTitle = Convert.ToString(dr["BlogTitle"]);
-            blog.BlogAuthor = Convert.ToString(dr["BlogAuthor"]);
-            blog.BlogContent = Convert.ToString(dr["BlogContent"]);
-            blogs.Add(blog);
-        }
+            BlogId = Convert.ToInt32(dr["BlogId"]),
+            BlogTitle = Convert.ToString(dr["BlogTitle"]),
+            BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            BlogContent = Convert.ToString(dr["BlogContent"])
+        }).ToList();
 
         return Ok(blogs);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBlogById(int id)
+    {
+        string query = "select * from tbl_blog where BlogId = @BlogId";
+
+        SqlConnection connection = new SqlConnection(SqlConnectionString.StringBuilder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogId", id);
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataTable data = new DataTable();
+        adapter.Fill(data);
+
+        connection.Close();
+
+        if (data.Rows.Count == 0)
+        {
+            return NotFound("Data not found in the table.");
+        }
+
+        DataRow dr = data.Rows[0];
+        var item = new BlogModel
+        {
+            BlogId = Convert.ToInt32(dr["BlogId"]),
+            BlogTitle = Convert.ToString(dr["BlogTitle"]),
+            BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            BlogContent = Convert.ToString(dr["BlogContent"])
+        };
+
+        return Ok(item);
     }
 
 }
