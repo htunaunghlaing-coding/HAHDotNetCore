c9de8d5058b8c3c1383dd5bb4c539f78d61495ce
hhtun137 <htunaunghlaing199@gmail.com>
2024-06-15 22:06:32 +0630

    writing custom dapper service

---
+++ HAHDotNetCore.Shared/DapperService.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace HAHDotNetCore.Shared;
+
+public class DapperService
+{
+    private readonly string _SqlConnectionString;
+
+    public DapperService(string sqlConnectionString)
+    {
+        _SqlConnectionString = sqlConnectionString;
+    }
+
+    public List<T> Query<T>(string query, Object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_SqlConnectionString);
+        List<T> list = db.Query<T>(query, param).ToList();
+        return list;
+    }
+
+    public int Execute(string query, Object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_SqlConnectionString);
+        var result = db.Execute(query, param);
+        return result;
+    }
+}
+
