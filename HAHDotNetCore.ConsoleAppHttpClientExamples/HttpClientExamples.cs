232906331d9a180e19bf10e08dd6ead5d1369040
hhtun137 <htunaunghlaing199@gmail.com>
2024-06-18 08:28:47 +0630

    writing console app read with api using HttpClient

---
+++ HAHDotNetCore.ConsoleAppHttpClientExamples/HttpClientExamples.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HAHDotNetCore.ConsoleAppHttpClientExamples;
+
+internal class HttpClientExamples
+{
+    private readonly HttpClient _client = new HttpClient() { BaseAddress = new Uri("https://localhost:7034") };
+    private readonly string _blogEndpoint = "api/blog";
+
+    public async Task RunAsync()
+    {
+        await ReadAsync();
+    }
+
+    private async Task ReadAsync()
+    {
+        var response = await _client.GetAsync(_blogEndpoint);
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonStr = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonStr);
+            List<BlogModel> lst = JsonConvert.DeserializeObject<List<BlogModel>>(jsonStr)!;
+            foreach (var blog in lst)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(blog));
+                Console.WriteLine($"BlogTitle => {blog.BlogTitle}");
+                Console.WriteLine($"BlogAuthor => {blog.BlogAuthor}");
+                Console.WriteLine($"BlogContent -> {blog.BlogContent}");
+            }
+        }
+    }
+}
+
---
+++ HAHDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HAHDotNetCore.ConsoleAppHttpClientExamples;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+HttpClientExamples httpClientExamples = new HttpClientExamples();
+await httpClientExamples.RunAsync();
