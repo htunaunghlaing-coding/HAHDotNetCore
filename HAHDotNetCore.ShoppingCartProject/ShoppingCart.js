dfcad54f168689240c8c9f2208a82efdfb8684f5
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-04 04:59:30 +0630

    writing update & delete for shopping cart

--- HAHDotNetCore.ShoppingCartProject/ShoppingCart.js
+++ HAHDotNetCore.ShoppingCartProject/ShoppingCart.js
@@ -1,6 +1,15 @@
 const tblProduct = "products";
 
-createProduct();
+// createProduct();
+// updateProduct(
+//   "60f2a5a2-f9df-4051-83ac-3e6b4538d13c",
+//   "Cheese Cake",
+//   "Bakery",
+//   1,
+//   "4000 MMK"
+// );
+deleteProduct("60f2a5a2-f9df-4051-83ac-3e6b4538d13c");
+
 
 function readProduct() {
   localStorage.getItem();
@@ -11,10 +20,11 @@ function createProduct() {
   console.log(products);
 
   const requestModel = {
+    id: uuidv4(),
     name: "Mini Croissant",
     category: "Bakery",
-    Quantity: "1",
-    Price: "5500 MMK",
+    quantity: 1,
+    price: "5500 MMK",
   };
 
   let list = [];
@@ -28,4 +38,65 @@ function createProduct() {
 
   const jsonProduct = JSON.stringify(list);
   localStorage.setItem(tblProduct, jsonProduct);
+}
+
+function updateProduct(id, name, category, quantity, price) {
+  const products = localStorage.getItem(tblProduct);
+  console.log(products);
+
+  let list = [];
+  if (products !== null) {
+    list = JSON.parse(products);
+  }
+
+  const items = list.filter((x) => x.id === id);
+  console.log(items);
+
+  console.log(items.length);
+
+  if (items.length == 0) {
+    console.log("Not data found in the table.");
+    return;
+  }
+
+  const item = items[0];
+  item.name = name;
+  item.category = category;
+  item.quantity = quantity;
+  item.price = price;
+
+  const index = list.findIndex((x) => x.id === id);
+  list[index] = item;
+
+  const jsonProduct = JSON.stringify(list);
+  localStorage.setItem(tblProduct, jsonProduct);
+}
+
+function deleteProduct(id) {
+  const products = localStorage.getItem(tblProduct);
+  console.log(products);
+
+  let list = [];
+  if (products !== null) {
+    list = JSON.parse(products);
+  }
+
+  const items = list.filter((x) => x.id === id);
+  if (items.length == 0) {
+    console.log("No Data Found in the table.");
+    return;
+  }
+
+  list = list.filter((x) => x.id !== id);
+  const jsonProduct = JSON.stringify(list);
+  localStorage.setItem(tblProduct, jsonProduct);
+}
+
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      +c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16)
+  );
 }
