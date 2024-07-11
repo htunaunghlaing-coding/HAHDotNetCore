e41c02b8976977bb483e763f22dcd90850760a57
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-11 13:43:44 +0630

    writing extension method for minimal api

---
+++ HAHDotNetCore.MinimalApi/Features/Blog/BlogService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using HAHDotNetCore.MinimalApi.Db;
+using HAHDotNetCore.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HAHDotNetCore.MinimalApi.Features.Blog;
+
+public static class BlogService
+{
+    public static IEndpointRouteBuilder MapBlog(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/Blog", async (AppDbContext db) =>
+        {
+            var lst = await db.Blogs.AsNoTracking().ToListAsync();
+            return Results.Ok(lst);
+        });
+
+        app.MapPost("api/Blog", async (AppDbContext db, BlogModel blog) =>
+        {
+            await db.Blogs.AddAsync(blog);
+            var result = await db.SaveChangesAsync();
+
+            string message = result > 0 ? "Saving Data Successfully." : "Saving Data Fail.";
+            return Results.Ok(message);
+        });
+
+        app.MapPut("api/Blog/{id}", async (AppDbContext db, int id, BlogModel blog) =>
+        {
+            var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Results.NotFound("Not found the data.");
+            }
+
+            item.BlogId = id;
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            var result = await db.SaveChangesAsync();
+
+            var message = result > 0 ? "Update Data Successfully." : "Update Data Fail.";
+            return Results.Ok(message);
+        });
+
+        app.MapDelete("api/Blog/{id}", async (AppDbContext db, int id) =>
+        {
+            var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Results.NotFound("Not found the data.");
+            }
+
+            db.Remove(item);
+            var result = db.SaveChanges();
+
+            var message = result > 0 ? "Delete Data Successfully." : "Delete Data Fail.";
+            return Results.Ok(message);
+        });
+
+        return app;
+    }
+}
+
--- HAHDotNetCore.MinimalApi/Program.cs
+++ HAHDotNetCore.MinimalApi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HAHDotNetCore.MinimalApi.Db;
+using HAHDotNetCore.MinimalApi.Features.Blog;
 using HAHDotNetCore.MinimalApi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,53 +29,8 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World");
 
-app.MapGet("api/Blog", async (AppDbContext db) =>
-{
-    var lst = await db.Blogs.AsNoTracking().ToListAsync();
-    return Results.Ok(lst);
-});
-
-app.MapPost("api/Blog", async (AppDbContext db, BlogModel blog) =>
-{
-    await db.Blogs.AddAsync(blog);
-    var result = await db.SaveChangesAsync();
-
-    string message = result > 0 ? "Saving Data Successfully." : "Saving Data Fail.";
-    return Results.Ok(message);
-});
-
-app.MapPut("api/Blog/{id}", async (AppDbContext db, int id, BlogModel blog) =>
-{
-    var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
-    if (item is null)
-    {
-        return Results.NotFound("Not found the data.");
-    }
-
-    item.BlogId = id;
-    item.BlogTitle = blog.BlogTitle;
-    item.BlogAuthor = blog.BlogAuthor;
-    item.BlogContent = blog.BlogContent;
-    var result = await db.SaveChangesAsync();
-
-    var message = result > 0 ? "Update Data Successfully." : "Update Data Fail.";
-    return Results.Ok(message);
-});
-
-app.MapDelete("api/Blog/{id}", async (AppDbContext db, int id) =>
-{
-    var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
-    if (item is null)
-    {
-        return Results.NotFound("Not found the data.");
-    }
-
-    db.Remove(item);
-    var result = db.SaveChanges();
-
-    var message = result > 0 ? "Delete Data Successfully." : "Delete Data Fail.";
-    return Results.Ok(message);
-});
+//BlogService.MapBlog(app);
+app.MapBlog();
 
 app.Run();
 
