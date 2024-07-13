d298ba791d7c7be676d015d006b146c9dbf4bb69
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-13 23:55:59 +0630

    configuring settings for asp.net core web mvc blog

---
+++ HAHDotNetCore.MvcApp/Db/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HAHDotNetCore.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HAHDotNetCore.MvcApp.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
+
