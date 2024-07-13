2f019b3c7ff96e07cd3c69f1de592465326ae202
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-13 14:55:13 +0630

    writing blog blazor assembly create

---
+++ HAHDotNetCore.BlazorWebAssemblyApp/Models/BlogModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HAHDotNetCore.BlazorWebAssemblyApp.Models;
+
+public class BlogModel
+{
+
+    public string? BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
+
