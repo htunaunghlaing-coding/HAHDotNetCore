ffc0e35d4e9ef9699e16aeaf760617213de37ba5
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-14 09:40:36 +0630

    writing asp.net core web mvc blog read

--- HAHDotNetCore.MvcApp/Controllers/BlogController.cs
+++ HAHDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HAHDotNetCore.MvcApp.Db;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,19 @@ namespace HAHDotNetCore.MvcApp.Controllers;
 
 public class BlogController : Controller
 {
-    // GET: /<controller>/
-    public IActionResult Index()
+    private AppDbContext _db;
+
+
+    public BlogController(AppDbContext db)
     {
-        return View();
+        _db = db;
+    }
+
+    // GET: /<controller>/
+    public async Task<IActionResult> Index()
+    {
+        var lst = await _db.Blogs.ToListAsync();
+        return View(lst);
     }
 }
 
