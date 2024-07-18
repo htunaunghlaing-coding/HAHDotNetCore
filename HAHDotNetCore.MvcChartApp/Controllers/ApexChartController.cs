a3a13d9ed543aadf402e7c6f7c0a501297ca9bdd
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-18 09:41:50 +0630

    writing asp.net core mvc pie chart

---
+++ HAHDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HAHDotNetCore.MvcChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HAHDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+
+        public IActionResult SimplePieChart()
+        {
+            PieChartModel pieChart = new PieChartModel();
+            pieChart.Labels = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+            pieChart.Series = new List<int>() { 44, 55, 13, 43, 22 };
+            return View(pieChart);
+        }
+    }
+}
+
---
+++ HAHDotNetCore.MvcChartApp/Models/PieChartModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HAHDotNetCore.MvcChartApp.Models;
+
+public class PieChartModel
+{
+
+    public List<int> Series { get; set; }
+    public List<string> Labels { get; set; }
+}
+
