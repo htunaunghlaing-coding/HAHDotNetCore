9f8ac1efc54c6969b577f9d5395e4c0c432a8681
hhtun137 <htunaunghlaing199@gmail.com>
2024-07-18 11:05:18 +0630

    writing asp.net core mvc interpolationlinechart of chartjs

--- HAHDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
+++ HAHDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -7,18 +7,17 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace HAHDotNetCore.MvcChartApp.Controllers
-{
-    public class ApexChartController : Controller
-    {
+namespace HAHDotNetCore.MvcChartApp.Controllers;
 
-        public IActionResult SimplePieChart()
-        {
-            PieChartModel pieChart = new PieChartModel();
-            pieChart.Labels = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
-            pieChart.Series = new List<int>() { 44, 55, 13, 43, 22 };
-            return View(pieChart);
-        }
+public class ApexChartController : Controller
+{
+
+    public IActionResult SimplePieChart()
+    {
+        PieChartModel pieChart = new PieChartModel();
+        pieChart.Labels = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+        pieChart.Series = new List<int>() { 44, 55, 13, 43, 22 };
+        return View(pieChart);
     }
 }
 
